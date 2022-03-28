2192c4632165afe11ce5684f388fc543fad4ee8b
wangxiao <eo_xiao@163.com>
2022-03-28 09:19:15 +0800

    fix(application): 去除启动类方法 多余注解

--- iotdm-application/src/main/java/com/aibaixun/iotdm/IotDmApplication.java
+++ iotdm-application/src/main/java/com/aibaixun/iotdm/IotDmApplication.java
@@ -2,16 +2,13 @@ package com.aibaixun.iotdm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * IOTdm 应用程序启动类
  * @author wangxiao@aibaixun.com
  * @date 2022/3/3
  */
-@EnableAsync
-@EnableScheduling
+
 @SpringBootApplication
 public class IotDmApplication {
 
