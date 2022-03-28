722ad0b13f3ee7621bcf363365ff95e4ff8b083b
wangxiao <eo_xiao@163.com>
2022-03-28 10:35:49 +0800

    fix(application): 去除启动类方法 多余注解

---
+++ iotdm-application/src/main/java/com/aibaixun/iotdm/entity/DeviceLinkRuleEntity.java
@@ -0,0 +1,17 @@
+package com.aibaixun.iotdm.entity;
+
+/**
+ * 设备联动
+ * @author wangxiao@aibaixun.com
+ * @date 2022/3/28
+ */
+public class DeviceLinkRuleEntity extends BaseEntity{
+
+    private String ruleLabel;
+
+    private String description;
+
+    private Boolean ruleStatus;
+
+
+}
